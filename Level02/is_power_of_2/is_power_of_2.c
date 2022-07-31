5a1aad6ca2dff20f0d0511d4a7c00a8ea660dfe6
Abdessalam Mounach <mounach-@hotmail.com>
2022-07-31 21:57:58 +0100

    refactor: improve logic

--- Level02/is_power_of_2/is_power_of_2.c
+++ Level02/is_power_of_2/is_power_of_2.c
@@ -1,30 +1,13 @@
-/*
-int	is_power_of_2(unsigned int n)
+int is_power_of_2(unsigned int n)
 {
-	unsigned int i = 1;
-	if (i < 1)
-		return (0);
-	else
-	{
-		while (i <= n)
-		{
-			if (i == n)
-				return (1);
-			i *= 2;
-		}
-	}
-	return (0);
-}*/
-
-int	next_power_of_2(int n)
-{
-	return (n << 2);
+	return (n != 0) && ((n & (n - 1)) == 0);
 }
+
 #include <stdio.h>
 int main ()
 {
-	unsigned int n = 8;
-	int a = next_power_of_2(n);
+	unsigned int n = 16;
+	int a = is_power_of_2(n);
 	printf ("%d", a);
 }
 
